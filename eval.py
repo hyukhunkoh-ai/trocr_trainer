03f0b982e1ed64b2c5285cc55a4eb5ffe0823a24
hhzzang <86921751+hhzzang@users.noreply.github.com>
2021-12-02 03:03:22 +0900

    Create eval.py

---
+++ eval.py
@@ -0,0 +1,15 @@
+#평가
+from datasets import load_metric
+
+metric = load_metric("accuracy")
+model.eval()
+for batch in eval_dataloader:
+    batch = {k: v.to(device) for k, v in batch.items()}
+    with torch.no_grad():
+        outputs = model(**batch)
+
+    logits = outputs.logits
+    predictions = torch.argmax(logits, dim=-1)
+    metric.add_batch(predictions=predictions, references=batch["labels"])
+
+metric.compute()
