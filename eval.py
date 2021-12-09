5735b3f823ff8b807d6e57c3c003098df744cc2e
hhzzang <86921751+hhzzang@users.noreply.github.com>
2021-12-09 23:09:59 +0900

    Update eval.py

--- eval.py
+++ eval.py
@@ -13,3 +13,27 @@ for batch in eval_dataloader:
     metric.add_batch(predictions=predictions, references=batch["labels"])
 
 metric.compute()
+
+
+
+def levenshtein(s1, s2, debug=False):
+    if len(s1) < len(s2):
+        return levenshtein(s2, s1, debug)
+
+    if len(s2) == 0:
+        return len(s1)
+
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1
+            deletions = current_row[j] + 1
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+
+        if debug:
+            print(current_row[1:])
+
+        previous_row = current_row
+    return previous_row[-1]
